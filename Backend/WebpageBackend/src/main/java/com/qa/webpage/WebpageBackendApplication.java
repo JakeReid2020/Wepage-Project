e33042644dc477701ad767157069a0c2accade17
jake.reid <jake.reid@qa.com>
2021-01-21 09:35:30 +0000

    Starting Backend

---
+++ Backend/WebpageBackend/src/main/java/com/qa/webpage/WebpageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qa.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpageBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/WebpageBackend/src/test/java/com/qa/webpage/WebpageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
